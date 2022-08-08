99e30f524f08cfe0bea07b8867139deea8505037
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2022-08-08 17:22:35 +0100

    Indenting the code

--- src/product/category/services/category.service.ts
+++ src/product/category/services/category.service.ts
@@ -4,25 +4,25 @@ import { CreateCategoryDto } from '../dto/create-category.dto';
 
 @Injectable()
 export class CategoryService {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async findAll(): Promise<{  data: any }> {
-        const categories = await this.prisma.category.findMany();
-        categories.map((category) => {
-            category.image = `http://localhost:3000/product/uploads/${category.image}`;
-        });
-        return { data: categories };
-    }
+  async findAll(): Promise<{ data: any }> {
+    const categories = await this.prisma.category.findMany();
+    categories.map((category) => {
+      category.image = `http://localhost:3000/product/uploads/${category.image}`;
+    });
+    return { data: categories };
+  }
 
-    async create(createCategoryDto: CreateCategoryDto): Promise<any> {
-        const category = await this.prisma.category.findMany({
-            where: {
-                name: createCategoryDto.name
-            }
-        });
-        if(category && category.length > 0) {
-            throw new BadRequestException('Categoria já existe');
-        }
-        return this.prisma.category.create({ data: createCategoryDto });
+  async create(createCategoryDto: CreateCategoryDto): Promise<any> {
+    const category = await this.prisma.category.findMany({
+      where: {
+        name: createCategoryDto.name,
+      },
+    });
+    if (category && category.length > 0) {
+      throw new BadRequestException('Categoria já existe');
     }
+    return this.prisma.category.create({ data: createCategoryDto });
+  }
 }
