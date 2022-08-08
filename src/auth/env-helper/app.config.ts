2f4005dd5b87baa39663eb50eb016865132589b9
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2022-08-08 12:50:35 +0100

    Removing heroku things

--- src/auth/env-helper/app.config.ts
+++ src/auth/env-helper/app.config.ts
@@ -1,5 +1,4 @@
 export default () => ({
   JWT_SECRET: process.env.JWT_SECRET,
-  baseURL: process.env.baseURL,
-  port: process.env.PORT
+  baseURL: process.env.baseURL
 });
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import appConfig from './auth/env-helper/app.config';
 
 
 async function bootstrap() {
-  const port = appConfig().port || 3000; // Por causa do deploy na heroku, não temos porta 3000, então vamos usar a porta que a heroku nos fornecer.
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true  })); // O Whitelist elimina/ignora da requisição todos aqueles dados que não estão no dto
   app.enableCors({
     origin: ['http://localhost:4200'],
     methods: ['POST', 'PUT', 'PATCH', 'DELETE', 'GET']
   });
-  await app.listen(port);
+  await app.listen(3000);
 }
 bootstrap();
